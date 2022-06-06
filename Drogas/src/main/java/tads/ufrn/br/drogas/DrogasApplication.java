e5bc84fad0dc0e3c0b8f2117871b8d97c12f198c
Anderso Henrique <dantasanderson57@hotmail.com>
2022-06-06 20:41:42 -0300

    Feito

--- Drogas/src/main/java/tads/ufrn/br/drogas/DrogasApplication.java
+++ Drogas/src/main/java/tads/ufrn/br/drogas/DrogasApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DrogasApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DrogasApplication.class, args);
+    public static void main(String[] args) { SpringApplication.run(DrogasApplication.class, args);
     }
 
 }
